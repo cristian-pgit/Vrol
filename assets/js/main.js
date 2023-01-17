235cf20de1d989441634d4d20914e2284a1f487e
Cristian Carrillo <naitsircl@gmail.com>
2023-01-17 13:44:49 -0300

    update 17/01

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function inscribeme() {
+    swal("VRol", "Inscrit@ Compi", "success", {button: "Gracias!"});
+ 
+}
+
+function averigua() {
+    swal("VRol", "Te contactaremos", "success", {button: "Vale!"});
+ 
+}
