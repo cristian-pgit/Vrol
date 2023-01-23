98fe07ebb4e50003fe00ffcecfac6b3e965b4d47
Cristian <naitsircl@gmail.com>
2023-01-22 21:30:39 -0300

    update fc

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ function averigua() {
     swal("VRol", "Te contactaremos", "success", {button: "Vale!"});
  
 }
+//reloj
+function startTime(){
+    today=new Date();
+    h=today.getHours();
+    m=today.getMinutes();
+    s=today.getSeconds();
+    m=checkTime(m);
+    s=checkTime(s);
+    document.getElementById('reloj1').innerHTML=h+":"+m+":"+s;
+    document.getElementById('reloj2').innerHTML=h+":"+m+":"+s;
+    t=setTimeout('startTime()',500);}
+    function checkTime(i)
+    {if (i<10) {i="0" + i;}return i;}
+    window.onload=function(){startTime();}
